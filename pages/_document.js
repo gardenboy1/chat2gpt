aa17cef95c5e86344ba241777735ac7271c537ed
gardenboy1 <117241668+gardenboy1@users.noreply.github.com>
2022-12-18 21:34:01 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Pun w/ GPT" key="title"/>
+        <meta property="og:description" content="Who'll have the last laugh?" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="http://cdn.onlinewebfonts.com/svg/img_182217.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
