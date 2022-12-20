27caf1bb82a5a1c56ecdf6c461a02a3289039f35
gardenboy1 <117241668+gardenboy1@users.noreply.github.com>
2022-12-19 20:52:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,16 +32,6 @@ const Home = () => {
     setIsGenerating(false);
     synth.speak(utterance);
   };
-  
-  if (window.speechSynthesis) {
-    // Use the SpeechSynthesis API to read the apiOutput text out loud
-    const synth = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance(apiOutput);
-    synth.speak(utterance);
-  } else {
-    // The SpeechSynthesis API is not supported in this browser
-    console.log('SpeechSynthesis is not supported in this browser');
-  };
 
   const onUserChangedText = (event) => {
   setUserInput(event.target.value);
